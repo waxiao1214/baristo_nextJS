ad3731b4a0c969f6b3a87af97a0c7ea527311e92
prince <wxiao1214@gmail.com>
2020-07-21 17:44:40 +0100

    Add: hero slider

--- components/pageSection/index/PageSectionIndexHero.js
+++ components/pageSection/index/PageSectionIndexHero.js
@@ -1,16 +1,42 @@
+import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 const PageSectionIndexHero = () => {
+    const [slides, setstate] = useState([]);
+
     return (<section class="banner">
         <div class="banner-slider">
-            <div class="banner-item" style={{ background: `url('images/picture/banner.png') no-repeat right bottom /cover` }}>
-                <div class="banner-text">
-                    <div class="container">
-                        <h4>We supply</h4>
-                        <h3>The Highest Quality And Tasty Food</h3>
-                        <button class="btn btn-h80 btn-yellow inflex-center-center" onclick="window.location.href=base_url + 'menu.html'">EXPLORE <i class="ti-arrow-right"></i> </button>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <div class="banner-item" style={{ background: `url('images/picture/banner.png') no-repeat right bottom /cover` }}>
+                        <div class="banner-text">
+                            <div class="container">
+                                <h4>We supply</h4>
+                                <h3>The Highest Quality And Tasty Food</h3>
+                                <button class="btn btn-h80 btn-yellow inflex-center-center" onclick="window.location.href=base_url + 'menu.html'">EXPLORE <i class="ti-arrow-right"></i> </button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div class="banner-item" style={{ background: `url('images/picture/banner.png') no-repeat right bottom /cover` }}>
+                        <div class="banner-text">
+                            <div class="container">
+                                <h4>We supply</h4>
+                                <h3>The Highest Quality And Tasty Food</h3>
+                                <button class="btn btn-h80 btn-yellow inflex-center-center" onclick="window.location.href=base_url + 'menu.html'">EXPLORE <i class="ti-arrow-right"></i> </button>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </div>
+
     </section>)
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux'
 import { useStore } from '../store'
 import '../assets/css/_main.css'
+import 'swiper/swiper.scss'
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState)
