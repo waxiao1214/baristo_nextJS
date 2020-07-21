4b0124f97185f365925244a1e7140b1efcdfe3ef
prince <wxiao1214@gmail.com>
2020-07-21 12:21:52 +0100

    Add: PageSectionIndexDeliveryAvailability

---
+++ components/pageSection/index/PageSectionIndexDeliveryAvailability.js
@@ -0,0 +1,18 @@
+const PageSectionIndexDeliveryAvailability = () => {
+    return (<section class="check-deliver pd-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <h4 class="text-center font-32 font-demi mgb-10">Looking Forward To Serve You</h4>
+                    <div class="desc font-20 text-center mgb-30">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit sapien a convallis lobortis. </div>
+                    <form>
+                        <input type="text" placeholder="2 Rue Notre-Dame, 2240 Luxembourg" class="input-white mgb-30" />
+                        <p class="text-center"><button type="submit" class="btn btn-white btn-h60">CHECK DELIVERY AVAILABILITY</button> </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>)
+}
+
+export default PageSectionIndexDeliveryAvailability;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import TheHeader from '../components/header/TheHeader'
 import TheFooter from '../components/footer/TheFooter'
 import PageSectionIndexChefsChoices from '../components/pageSection/index/PageSectionIndexChefsChoices'
 import PageSectionIndexSpecialCruise from '../components/pageSection/index/PageSectionIndexSpecialCruise'
+import PageSectionIndexDeliveryAvailability from '../components/pageSection/index/PageSectionIndexDeliveryAvailability'
 
 export default function Index() {
   return (
     <div>
       <TheHeader />
       <PageSectionIndexSpecialCruise />
+      <PageSectionIndexDeliveryAvailability />
       <PageSectionIndexChefsChoices />
       <TheFooter />
     </div>
