14290e2c6cba2c82fb53564080903e18b3fc7c40
prince <wxiao1214@gmail.com>
2020-07-23 14:13:01 +0100

    Add: bg image to load from API

--- components/pageSection/index/PageSectionIndexOurRestaurant.js
+++ components/pageSection/index/PageSectionIndexOurRestaurant.js
@@ -1,28 +1,31 @@
 const PageSectionIndexOurRestaurant = ({ subBanner }) => {
-    return (<section className="our-restaurant">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6 offset-md-6">
-                    <div className="our-res-right">
-                        <h2 className="title text-left">
-                            <span>{subBanner.title}</span>
-                        </h2>
-                        <div className="desc font-20">
-                            {subBanner.description }
+    return (
+        <section
+            className="our-restaurant"
+            style={{ 'backgroundImage': `url(${subBanner.mediaLink})`}}
+        >
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 offset-md-6">
+                        <div className="our-res-right">
+                            <h2 className="title text-left">
+                                <span>{subBanner.title}</span>
+                            </h2>
+                            <div className="desc font-20">
+                                {subBanner.description}
+                            </div>
+                            <p className="our-button">
+                                <button
+                                    className="btn btn-white btn-h60 font-demi"
+                                >
+                                    {subBanner.actionButtonText}
+                                </button>
+                            </p>
                         </div>
-                        <p className="our-button">
-                            <button
-                                herf={subBanner.mediaLink}
-                                className="btn btn-white btn-h60 font-demi"
-                            >
-                                {subBanner.actionButtonText}
-                            </button>
-                        </p>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>)
+        </section>)
 }
 
 export default PageSectionIndexOurRestaurant;
