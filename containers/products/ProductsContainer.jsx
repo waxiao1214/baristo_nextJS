9ec190ff700b111b867c43c4f95f39c4170da027
prince <wxiao1214@gmail.com>
2020-08-05 15:12:05 +0100

    Style: ProductsContainer to be responsive

--- containers/products/ProductsContainer.jsx
+++ containers/products/ProductsContainer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ProductCard from '../../components/product/ProductCard';
+import _ from 'lodash';
 
 const ProductsContainer = ({ products }) => {
+	if (!_.isArray(products)) return '';
+
 	return (
 		<div className="row">
 			{products.map((product) => (
-				<ProductCard product={product} />
+				<div className="col-md-4" key={product.id}>
+					<ProductCard product={product}/>
+				</div>
 			))}
 		</div>
 	);
