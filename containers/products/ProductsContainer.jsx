44ac37862074a16d67d08e4f8e172d6fc36e3c50
prince <wxiao1214@gmail.com>
2020-08-05 14:57:49 +0100

    Add: ProductsContainer component

--- containers/products/ProductsContainer.jsx
+++ containers/products/ProductsContainer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import ProductCard from '../../components/product/ProductCard';
 
-const ProductsContainer = () => {
-    return ();
-}
+const ProductsContainer = ({ products }) => {
+	return (
+		<div className="row">
+			{products.map((product) => (
+				<ProductCard product={product} />
+			))}
+		</div>
+	);
+};
 
 export default ProductsContainer;
