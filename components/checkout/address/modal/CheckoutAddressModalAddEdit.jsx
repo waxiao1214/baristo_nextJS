df3d619d3573d2743209a76a32e308730e91b854
prince <wxiao1214@gmail.com>
2020-08-25 09:18:08 +0100

    progress

--- components/checkout/address/modal/CheckoutAddressModalAddEdit.jsx
+++ components/checkout/address/modal/CheckoutAddressModalAddEdit.jsx
@@ -1,5 +1,31 @@
-const CheckoutAddressModalAddEdit = () => {
-    return ();
+const CheckoutAddressModalAddEdit = ({ close }) => {
+    return (
+        <div>
+            <div className="modal fade modal-box show" id="search-filter" role="dialog" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-top">
+                            <h2 className="title">
+                                <span>Add new address</span>
+                            </h2>
+                            <button
+                                onClick={close} type="button" className="close" data-dismiss="modal" aria-label="Close"><i className="ti-close" /> </button>
+                        </div>
+                        <div className="modal-main">
+                            <div className="container">
+                                <div className="cols">
+                                    <div className="col-12">
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal-backdrop fade show" />
+        </div>
+    );
 }
 
 export default CheckoutAddressModalAddEdit;
---
+++ components/modal/branch/ModalChangeBranch.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import Link from 'next/link'
+
+const ModalChangeBranch = ({ close, isActive }) => {
+  const { t } = useTranslation(['common']);
+  const { branches } = useSelector((state) => state.root.settings);
+
+  if (!isActive) {
+    return '';
+  }
+
+  return (
+    <div>
+      <div className="modal fade modal-box show" id="search-filter" role="dialog" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-top">
+              <h2 className="title">
+                <span>{t('change_the_branch')}</span>
+              </h2>
+              <button
+                onClick={close} type="button" className="close" data-dismiss="modal" aria-label="Close"><i className="ti-close" /> </button>
+            </div>
+            <div className="modal-main">
+              <div className="container">
+                <div className="cols">
+                  <div className="col-12">
+                    {branches.map(branch => {
+                      return (
+                        <Link href={`/${branch.primaryBranch ? '' : branch.id}`}>
+                          <a className="btn btn-link mx-auto btn--no-shadow w-100 mb-2">{branch.branchName}</a>
+                        </Link>
+                      );
+                    })
+                    }
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="modal-backdrop fade show" />
+    </div>
+  )
+}
+
+export default ModalChangeBranch;
